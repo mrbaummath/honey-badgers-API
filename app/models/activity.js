a1a39107022f9fa0b98d8b367a1624953c46fd1b
lozier <latayroutier@gmail.com>
2022-10-29 15:21:53 -0400

    version 1 of activitySchema complete

---
+++ app/models/activity.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const activitySchema = new mongoose.Schema(
+    {
+        activity: {
+            type: String,
+            required: true
+        },
+        accessibility: {
+            type: Number,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true,
+            enum: ['education', 'social', 'diy', 'charity', 'cooking', 'relaxation', 'music', 'busywork'],
+        },
+        participants: {
+            type: Number,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        buddies: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Activity', activitySchema)
