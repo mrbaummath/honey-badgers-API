65171b6a4729f31c90f73b7329b891f48ba8fffd
lozier <latayroutier@gmail.com>
2022-10-29 20:22:06 -0400

    Route complete with functionality issues

---
+++ app/models/seed.js
@@ -0,0 +1,40 @@
+// added this seed file to test all activity routes
+
+const mongoose = require('mongoose')
+const Activity = require('./activity')
+const db = require('../../config/db')
+
+const startActivities = [
+    { activity: 'Take a bubble bath', accessibility: 0.1, type: 'relaxation', participants: 1, price: 0.15 },
+    { actvity: 'Clean out sock drawer', accessibility: 0, type: 'busywork', participants: 1, price: 0 },
+    { activity: 'Host a board game night', accessibility: 0.5, type: 'social', participants: 4, price: 0.1}
+]
+
+// connect to database
+mongoose.connect(db, {
+    useNewUrlParser: true
+})
+    .then(() => {
+        Activity.deleteMany({ owner: null })
+            .then(deletedActivities => {
+                console.log('deletedActivities', deletedActivities)
+
+                Activity.create(startActivities)
+                    .then(newActivities => {
+                        console.log('the new activities', newActivities)
+                        mongoose.connection.close()
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        mongoose.connection.close()
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+                mongoose.connection.close()
+            })
+    })
+    .catch(error => {
+        console.log(error)
+        mongoose.connection.close()
+    })
